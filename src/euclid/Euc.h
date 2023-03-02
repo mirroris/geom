2811e5e447999b6a2cde390949c9e267445d9261
mirroris <u455017k@ecs.osaka-u.ac.jp>
2023-03-03 00:45:26 +0900

    add enum for expression

--- src/euclid/Euc.h
+++ src/euclid/Euc.h
@@ -1,11 +1,23 @@
+enum func {
+    CONST,
+    SIN,
+    COS,
+    LOG,
+    EPOW,
+    POW
+};
 
 class Euc {
     private:
     float begin;
     float end;
+    func type;
+    vector<Euc> childs;
     public:
     Euc();
     Euc(float begin, float end);
+    Euc(func type);
     ~Euc();
     void assum(float begin, float end);
+    void build(func type);
 };
